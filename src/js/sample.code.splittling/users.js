e27286a526110bdcdb98ddea4ec1cafe90b68cae
peter barraud <gapeterb@gmail.com>
2024-12-08 21:11:19 +0530

    addeds simple rest fetch methods for POST and GET

--- src/js/sample.code.splittling/users.js
+++ src/js/sample.code.splittling/users.js
@@ -1,4 +1,39 @@
 const validateUser = (username, password) => {
-    console.log(`User name: ${username}; Password: ${password}`);    
+
+    if (username && password){
+        fetch(`${restURL}/validuser/`, {
+        method: "POST",
+        body: JSON.stringify({
+            userdata: {
+            username: username,
+            password: password
+            }
+        })})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })  
+    } else {
+        alert("User name & Pwd requried");
+    }
+
 };
-export {validateUser};
+const getUsers = () => {
+    fetch(`${restURL}/getuserlist/`, {
+            method: "GET",
+        })
+        .then(response => response.json())
+        .then(data => {
+            let users = '';
+            data.forEach(user => {
+                console.log(user.username);
+        });
+        })
+        .catch((err) => {
+            console.log(err);
+        })     
+}
+export {validateUser, getUsers};
